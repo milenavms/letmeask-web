9da7e80806038631a81e534e79195fe270d2564d
milena <milenasantos@bemol.com.br>
2021-06-23 21:10:58 -0400

    Tela de criação de sala

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
+import { Button } from '../componentes/Button'
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie sala de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="letmeask" />
+                    <h2>Crie uma nova sala</h2>
+                    <form action="">
+                        <input
+                            type="text"
+                            placeholder="Nome da Sala"
+                        />
+                        <Button type="submit">
+                            Criar Sala
+                        </Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existente? <a href="#"> clique aqui </a>
+                    </p>
+                </div>
+            </main>
+
+        </div>
+    )
+}
